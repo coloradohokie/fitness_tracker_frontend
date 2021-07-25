27e8fa038e1c2bd9284b6d1bf37dcfee14681ac9
Michael Newman <coloradohokie@gmail.com>
2021-07-25 14:59:55 -0600

    add filtering and sorting to My Activity History

--- src/components/ActivityHistory/ActivityHistory.js
+++ src/components/ActivityHistory/ActivityHistory.js
@@ -7,16 +7,24 @@ const activityHistory = (props) => {
     const printActivityItem = () => (
         props.activityHistory
             .filter(item => item.user_id === parseInt(localStorage.getItem('userId')))
+            .sort((a,b) => new Date(a.date).getTime() - new Date(b.date).getTime())
             .map((item, index) => (
                 <ActivityItem 
                     key = {index}
                     activity= {item.name}
                     date={item.date}
                     distance = {(item.distance / 5280).toFixed(2) }
-                    duration = {item.duration}                
+                    duration = {item.duration}  
+                    weekNumber = {setWeekNumber(item.date)}              
                 />
             ))
     )
+
+    const setWeekNumber = (activityDate) => {
+        const startDate = new Date ('2021-07-16 0:00:00 GMT-6:00')
+        const millisecondsInAWeek = 604800000
+        return Math.trunc((new Date(activityDate).getTime() - startDate.getTime())/millisecondsInAWeek) + 1
+    }
 
     return(
         <Table>
